93dd7553ce8827cc54dec58601cc92cd438a22bf
Cody Keener <codymkeener@gmail.com>
2024-06-03 21:04:30 -0500

    Created all functions to get HTML on the DOM

---
+++ components/shared/domBuilder.js
@@ -0,0 +1,14 @@
+import renderToDom from '../../utils/renderToDom';
+
+const domBuilder = () => {
+  const domString = `
+  <div id ="navigation"></div>
+  <div id ="main-container">
+    <div id="vocab-cards-container"></div>
+  </div>
+  `;
+
+  renderToDom('#app', domString);
+};
+
+export default domBuilder;
